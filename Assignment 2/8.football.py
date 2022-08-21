6a14ad9ed62307f561d05061045f2cad4a446ad7
Mohammad Hossein Keykhaee <110756265+MohammadHosseinKeykhaee@users.noreply.github.com>
2022-08-21 20:02:33 +0430

    Update 8.football.py

--- Assignment 2/8.football.py
+++ Assignment 2/8.football.py
@@ -1,23 +1,25 @@
-#football
 match_number=0
+result=0
 Lose=0
 Draw=0
 Win=0
-result=0
 while match_number<8:
-    print("GAME" ,match_number+1, "Please input Result Game: ""\nwin : w  lose : l  draw : d ")
+
+    print("Result Game" ,match_number+1, "?? "
+    "\nWin : w  Lose : l  Draw : d ")
     G=input()
-    if G=="W" or "w":
-        result=result + 3
-        Win= Win +1
-    elif G=="D" or "d":
-        result=result + 1
-        Draw=Draw+1
-    elif G=="L" or "l":
-        result=result + 0
-        Lose=Lose+ 1
+    if G=="w":
+        result+=3
+        Win+=1
+    elif G=="d":
+        result+=1
+        Draw+=1
+    elif G=="l":
+        result+=0
+        Lose+=1
     else:
-        match_number=match_number-1
+        match_number-=1
         print("Try again")
     match_number+=1
-print("Result in 8 matchs : ",Win,"win",Draw,"draw", Lose,"lose" "\nFinal points : ",result )
+print("Result in 8 matchs is :",Win,"Win",Draw,"Draw", Lose,"Lose"
+   "\nFinal ponits : ",result )
